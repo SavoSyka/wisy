405ad4dd4a59e8730b5431ab09a94e30ecc75ed1
SavoSyka <savostikov.artem@gmail.com>
2023-06-18 21:15:38 +0400

    ETH added

--- binance_rate.py
+++ binance_rate.py
@@ -7,25 +7,27 @@ from matplotlib import pyplot as plt
 apikey = 'L9unNk9gLdXRQnwo869ZptQONUW7kwfZktfjg2YXMrhdlCB4LI5Ba2pJ2mzo4X0b'
 secret = 'au1XYYo13c6YVmZY8XKzelYxA6GRZY5DyJOrd73Nkmtesm7gA8C8UT7geFlQ9WpN'
 client = Client(apikey, secret)
-data = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Dec, 2017", "17 Jun, 2023")
-BTC = pd.DataFrame(data)
+BTC_data = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Dec, 2017", "17 Jun, 2023")
+BTC = pd.DataFrame(BTC_data)
 
 BTC.columns = ['date', 'open', 'high', 'low', 'close', '5', '6', '7', '8', '9', '10', '11']
 
-
-# ts = df.loc[0,'date']
-# print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-# print(ts)
 for i in range (0,2025):
     BTC.loc[i, 'date'] = datetime.utcfromtimestamp(BTC.loc[i, 'date'] / 1000).strftime('%Y-%m-%d')
 print(BTC.tail())
-#print(datetime.fromtimestamp(df.loc[0,'date']))
-# ts = int('1512086400')
-# print(df.loc[0,'date']/1000)
-# print(datetime.utcfromtimestamp(df.loc[0,'date']/1000).strftime('%Y-%m-%d'))
-
-
-
 
 BTC.to_excel('BTC_USDT_01122017-17062023.xlsx', index=False)
 BTC.to_csv('BTC_USDT_01122017-17062023.csv', index=False)
+
+
+ETH_data = client.get_historical_klines("ETHUSDT", Client.KLINE_INTERVAL_1DAY, "1 Dec, 2017", "17 Jun, 2023")
+ETH = pd.DataFrame(ETH_data)
+
+ETH.columns = ['date', 'open', 'high', 'low', 'close', '5', '6', '7', '8', '9', '10', '11']
+
+for i in range (0,2025):
+    ETH.loc[i, 'date'] = datetime.utcfromtimestamp(ETH.loc[i, 'date'] / 1000).strftime('%Y-%m-%d')
+print(ETH.tail())
+
+ETH.to_excel('ETH_USDT_01122017-17062023.xlsx', index=False)
+ETH.to_csv('ETH_USDT_01122017-17062023.csv', index=False)
