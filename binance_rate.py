57518a7a35a77c493c853a250134ee5d3e97c7c0
SavoSyka <savostikov.artem@gmail.com>
2023-06-18 21:11:13 +0400

    added normal date

--- binance_rate.py
+++ binance_rate.py
@@ -1,6 +1,6 @@
 from binance import Client, ThreadedDepthCacheManager, ThreadedWebsocketManager
 import pandas as pd
-
+from datetime import datetime
 import openpyxl
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,11 +8,24 @@ apikey = 'L9unNk9gLdXRQnwo869ZptQONUW7kwfZktfjg2YXMrhdlCB4LI5Ba2pJ2mzo4X0b'
 secret = 'au1XYYo13c6YVmZY8XKzelYxA6GRZY5DyJOrd73Nkmtesm7gA8C8UT7geFlQ9WpN'
 client = Client(apikey, secret)
 data = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Dec, 2017", "17 Jun, 2023")
-df = pd.DataFrame(data)
+BTC = pd.DataFrame(data)
 
-df.columns = ['date', 'open', 'high', 'low', 'close','5','6','7','8','9','10','11']
+BTC.columns = ['date', 'open', 'high', 'low', 'close', '5', '6', '7', '8', '9', '10', '11']
 
-df.to_excel('BTC_USDT_01122017-17062023.xlsx', index=False)
-df.to_csv('BTC_USDT_01122017-17062023.csv', index=False)
 
-print(df.head())
+# ts = df.loc[0,'date']
+# print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
+# print(ts)
+for i in range (0,2025):
+    BTC.loc[i, 'date'] = datetime.utcfromtimestamp(BTC.loc[i, 'date'] / 1000).strftime('%Y-%m-%d')
+print(BTC.tail())
+#print(datetime.fromtimestamp(df.loc[0,'date']))
+# ts = int('1512086400')
+# print(df.loc[0,'date']/1000)
+# print(datetime.utcfromtimestamp(df.loc[0,'date']/1000).strftime('%Y-%m-%d'))
+
+
+
+
+BTC.to_excel('BTC_USDT_01122017-17062023.xlsx', index=False)
+BTC.to_csv('BTC_USDT_01122017-17062023.csv', index=False)
